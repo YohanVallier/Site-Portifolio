1ad93620abcf68c1af26a90fe16f31c1f3b265a6
Yohan Vallier <162353277+YohanVallier@users.noreply.github.com>
2025-03-25 15:43:13 -0300

    mudança mobile

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,15 @@
-function envioFormulario() {
+document.querySelector('.formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    
+    const mensagemDiv = document.getElementById('mensagem');    
+    document.getElementById('inome').value = ""; // Clear name input
+    document.getElementById('iemail').value = ""; // Clear email input
+    document.getElementById('inumero').value = ""; // Clear phone input
 
-}
+    mensagemDiv.style.opacity = "1";
+    mensagemDiv.style.gap = "10px";
+
+    setTimeout(() => {
+        mensagemDiv.style.opacity = "0"; mensagemDiv.style.transition = "opacity 0.3s"}, 1500);
+    });
