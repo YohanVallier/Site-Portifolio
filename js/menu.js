eb1be7ebdc93ca9d43c50a30ed5bbd1f4161f3a0
Yohan Vallier <162353277+YohanVallier@users.noreply.github.com>
2025-03-25 16:49:52 -0300

    mudança mobile

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,6 @@ function acaoMenu() {
     const navbar = document.querySelector(".navbar");
     const wrapper = document.querySelector('.menu-wrapper');
     menu.classList.toggle('show'); 
-    menu.style.marginRight = "15%";
+    menu.style.marginRight = "8%";
     wrapper.style.marginLeft = "80%";
 };
