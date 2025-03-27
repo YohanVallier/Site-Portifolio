bd51ceed0a372f6e033302d9de3b17c271fc7e21
Yohan Vallier <162353277+YohanVallier@users.noreply.github.com>
2025-03-27 11:24:55 -0300

    alteração menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,7 @@
 function acaoMenu() {
     const menu = document.querySelector('.menu');
-    const wrapper = document.querySelector('.menu_wrapper');
-    const botão = document.querySelector('.menu_botão img');
+    const nav = document.querySelector('nav');
 
     menu.classList.toggle('show');
-    if (menu.classList.contains('show')) {
-        wrapper.style.display = 'flex';
-        wrapper.style.flexDirection = 'column';
+}
 
-    } else {
-        wrapper.style.display = 'flex';
-        wrapper.style.flexDirection = 'column';
-    }
-
-};
